5b7cdaefa3f80589c7587b84e3ed655c92a4ad9c
Tutoro <giorgiogino0@gmail.com>
2017-05-02 08:30:57 +0200

    Add files via upload

---
+++ Monopoly/Monopoly/Classi/Proprieta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Classi
+{
+
+    enum Tipo_Speciali
+    { Imprevisti, Parcheggio, Prigione, Probabilita, Tassa };
+
+    class Proprieta
+    {
+
+    }
+
+    class Speciali
+    {
+
+    }
+
+    class Proprieta_Speciali
+    {
+
+    }
+}

1cbbe62b06128b011a057429a63ae0764aaaec15
ZaccAn <zacc.andri@gmail.com>
2017-05-02 09:38:56 +0200

    Add files via upload

--- Monopoly/Monopoly/Classi/Casella.cs
+++ Monopoly/Monopoly/Classi/Casella.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Monopoly.Classi
 {
-    class Casella
+    class Casella : Image
     {
+        public Brush Colore;
+        public Casella(Brush Col)
+        {
+            Colore = Col;
+        }
     }
 }

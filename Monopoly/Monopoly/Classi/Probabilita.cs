39e2b6a18da336e944138fe587185960177104c2
HexagonZzZ <claudio.verazza@hotmail.it>
2017-05-19 13:20:33 +0200

    Probab
    aggiunti testi e robe

--- Monopoly/Monopoly/Classi/Probabilita.cs
+++ Monopoly/Monopoly/Classi/Probabilita.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Monopoly.Classi
 {
     public enum Tipo_Probabilita
-    { SpostaCasella, SpostaNumero, Tassa, TassaGlobale, UscitaPrigione };
+    { SpostaCasella, SpostaNumero, Tassa, TassaGlobale, UscitaPrigione, TassaCaseAlberghi };
 
     class Carta
     {
@@ -15,22 +15,63 @@ namespace Monopoly.Classi
         public int Pagamento;
         public int Spostamento;
         public bool Via;
+        public int PagaC; // Costo per ogni casa
+        public int PagaA; // Costo per ogni albergo
+        public string Messaggio;
 
-        public Carta(Tipo_Probabilita T, int P)
+        public Carta(Tipo_Probabilita T, int P, string M)
         {
             Tipo = T;
             Pagamento = P;
+            Messaggio = M;
         }
-        public Carta(Tipo_Probabilita T, int S, bool V)
+        public Carta(Tipo_Probabilita T, int S, bool V, string M)
         {
             Tipo = T;
             Spostamento = S;
             Via = V;
+            Messaggio = M;
         }
-        public Carta(Tipo_Probabilita T)
+        public Carta(Tipo_Probabilita T, string M)
         {
             Tipo = T;
+            Messaggio = M;
         }
 
+        public Carta(Tipo_Probabilita T, int C, int A, string M)
+        {
+            Tipo = T;
+            PagaC = C;
+            PagaA = A;
+            Messaggio = M;
+        }
+
+        public static void Scorri(ref Carta[] L)
+        {
+            Carta T = L[0];
+            for (int i = 1; i < L.Length; i++)
+                L[i - 1] = L[i];
+
+            L[L.Length - 1] = T;
+        }
+
+        public static void RimuoviPrigione(ref Carta[] L)
+        {
+            Carta[] T = new Carta[L.Length - 1];
+            for (int i = 1; i < L.Length; i++)
+                    T[i - 1] = L[i];
+
+            L = T;
+        }
+
+        public static void AggiungiPrigione(ref Carta[] L)
+        {
+            Carta[] T = new Carta[L.Length + 1];
+            for (int i = 0; i < L.Length; i++)
+                T[i] = L[i];
+
+            T[T.Length - 1] = new Carta(Tipo_Probabilita.UscitaPrigione, "");
+            L = T;
+        }
     }
 }
